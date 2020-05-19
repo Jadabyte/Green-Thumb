0f8915c76dd7557d65276255d355c74a7547be79
Jadabyte <Jadabyte>
2020-05-19 18:19:35 +0200

    adding

---
+++ nav.inc.php
@@ -0,0 +1,14 @@
+<div id="plants">
+    <a href="index.php"><img src="" alt="My Plants"></a>
+    <p>My Plants</p>
+</div>
+
+<div id="platform">
+    <a href="platform.php"><img src="" alt="Platform"></a>
+    <p>Platform</p>
+</div>
+
+<div id="profile">
+    <a href="profile.php"><img src="" alt="Profile"></a>
+    <p>Profile</p>
+</div>
--- register.php
+++ register.php
@@ -1,15 +1,17 @@
 <?php
 
 include_once(__DIR__ . "/Classes/User.php");
-if(!empty($_POST)){
-    try {
-        $user = new User;
-        $user->setFirstname($_POST['firstname']);
-        $user->setLastname($_POST['lastname']);
-        $user->setEmail($_POST['email']);
-        $user->setPassword($_POST['password']);
 
-        var_dump($_POST['firstname']);
+if(isset($_POST['reg_firstname'])){
+    try {
+        echo("set");
+        $user = new User;
+        $user->setFirstname($_POST['reg_firstname']);
+        $user->setLastname($_POST['reg_lastname']);
+        $user->setEmail($_POST['reg_email']);
+        $user->setPassword($_POST['reg_password']);
+
+        $user->createUser();
     } catch (\Exception $e) {
         $error = $e->getMessage();
     }
@@ -31,25 +33,25 @@ if(!empty($_POST)){
         <p id="subHead">Sign in to continue</p>
     </header>
     <main id="registerForm">
-        <form action="" method="post">
+        <form id="form" action="landing.php" method="post">
             <div class="firstname hidden">
                 <label for="Firstname"></label>
-                <input type="text" id="Firstname" name="firstname" placeholder="Firstname">
+                <input type="text" id="Firstname" name="reg_firstname" placeholder="Firstname">
             </div>
 
             <div class="lastname hidden">
                 <label for="Lastname"></label>
-                <input type="text" id="Lastname" name="lastname" placeholder="Lastname">
+                <input type="text" id="Lastname" name="reg_lastname" placeholder="Lastname">
             </div>
 
             <div class="email">
                 <label for="Email"></label>
-                <input type="text" id="Email" name="email" placeholder="Email">
+                <input type="text" id="Email" name="reg_email" placeholder="Email">
             </div>
 
             <div class="password">
                 <label for="Password"></label>
-                <input type="password" id="Password" name="password" placeholder="Password">
+                <input type="password" id="Password" name="reg_password" placeholder="Password">
                 <p id="forgot">Forgot your password?</p>
             </div>
 
@@ -59,7 +61,7 @@ if(!empty($_POST)){
             </div>
 
             <div>
-                <input type="submit" id="submit" value="Continue">
+                <input id="submit" type="submit" value="Continue">
             </div>
         </form>
     </main>
