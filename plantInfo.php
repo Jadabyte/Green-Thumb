3ec674d4e602ea73a50c12ccf75d6abde61a4b45
Jadabyte <thibaud.streignart@gmail.com>
2020-06-14 09:26:59 +0200

    plant guide layout finished, css needs to be added

--- plantInfo.php
+++ plantInfo.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+    include_once(__DIR__ . "/nav.inc.php");
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -15,17 +17,25 @@
         <h2 class="plantName">Tomato</h2>
     </header>
     <main>
-        <p>Care level: intermediate</p>
-        <div id="infoCards">
-            <div class="infoCard">
-                <h2 class="cardTitle">Planting</h2>
+        <p id="careLvl">Care level: intermediate</p>
+        <div class="guideBox">
+            <div id="planting" class="plantCard">
+                <h2 id="plantingTitle" class="cardTitle">Planting</h2>
                 <p class="cardContent">Use a large pot or container with drainage holes in the bottom. Use loose, well-draining soil. Mix organic matter with a good potting mix. Only plant 1 tomato plant per pot. Keep the soil moist. Place the pot in a sunny spot with 6 to 8 hours of full sun a day.</p>
             </div>
         </div>
-        <div id="infoCards">
+        
+        <div id="careCards">
             <div class="infoCard">
-                <h2 class="cardTitle">Planting</h2>
-                <p class="cardContent">Use a large pot or container with drainage holes in the bottom. Use loose, well-draining soil. Mix organic matter with a good potting mix. Only plant 1 tomato plant per pot. Keep the soil moist. Place the pot in a sunny spot with 6 to 8 hours of full sun a day.</p>
+                <h2 class="cardTitle">Care</h2>
+                <h3 class="subTitle">Watering</h3>
+                <p class="cardContent">Water generously the first few days thatthe tomato plant is in the ground.Water well throughout growing season.Give water in the early morning.Add mulch after 5 weeks to retain moistureand to control weeds.</p>
+            </div>
+            <div class="guideBox">
+                <div class="infoCard">
+                    <h3 class="subTitle">Fertilizing</h3>
+                    <p class="cardContent">Water in with a starter fertilizer solution.Side dress the plants with ferilizer or compostevery 2 weeks when tomato fruits areabout 3 cm in diameter.If staking, use soft string to securetomato stem to the stake.</p>
+                </div>
             </div>
         </div>
     </main>
