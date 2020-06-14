ba7811a878158231640c80428a4e6d878e4fe04e
Jadabyte <thibaud.streignart@gmail.com>
2020-06-14 17:51:26 +0200

    wrote a  proper layout for plantSensor.php and some checks for the plant parameters

--- plantSensor.php
+++ plantSensor.php
@@ -19,9 +19,106 @@ if(isset($_POST["data"])){ // Checks if $_POST['data'] exists
 
 $result = PlantData::retrieveData(); // Retrieves the most recent entry from the database
 
+$celcius = $result['celcius'];
+    if($celcius < 15) { 
+        $tempHead = "Low";
+        $tempContent = "It's too cold, find a warmer spot";
+    }
+    else if($celcius > 15 && $celcius < 24) { 
+        $tempHead = "Good";
+        $tempContent = "The temperature in the room is optimal!";
+    }
+    else {
+        $tempHead = "High";
+        $tempContent = "It's too warm, find a cooler spot";
+    }
+$fahrenheit = $result['fahrenheit'];
+
+$light = $result['light'];
+    if($result['light'] < 333){
+        $brightnessHead = "Low";
+        $brightnessContent = "Your plant isn't receiving enough sunlight";
+        $brightness = "Dark";
+    }
+    else if($result['light'] > 333 && $result['light'] < 666){
+        $brightnessHead = "Good";
+        $brightnessContent = "Your plant is receiving enough sunlight!";
+        $brightness = "Bright";
+    }
+    else if($result['light'] > 666){
+        $brightnessHead = "High";
+        $brightnessContent = "Your plant is receiving too much sunlight";
+        $brightness = "Extremely Bright";
+    }
+
+$moisture = $result['moisture'];
+    if($result['moisture'] < 50){
+        $waterHead = "Low";
+        $waterState = "Your plant needs to be watered";
+    }
+    else{
+        $waterHead = "Normal";
+        $waterState = "All good";
+    }
+
 /* These print the data onto the webpage */
-echo("Celcius: " . $result['celcius'] . "<br></br>");
+/*echo("Celcius: " . $result['celcius'] . "<br></br>");
 echo("Fahrenheit: " . $result['fahrenheit'] . "<br></br>");
-echo("Brightness: " . $result['light'] . "<br></br>");
-echo("Moisture: " . $result['moisture'] . "<br></br>");
-echo("Date of Measurement: " . $result['time'] . "<br></br>");
+echo("Brightness: " . $result['light'] . " - " . $brightness . "<br></br>");
+echo("Moisture: " . $result['moisture'] . " - " . $waterState . "<br></br>");
+echo("Date of Measurement: " . $result['time'] . "<br></br>");*/
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
+    <link rel="stylesheet" href="https://use.typekit.net/ayy1bcm.css">
+    <link rel="stylesheet" href="css/plantSensor.css">
+    <title><?php ?> Details</title>
+</head>
+<body>
+    <header class="plant" id="montsera" data-name="monstera">
+        <h2 class="plantName">Plant Monitor</h2>
+    </header>
+    <main>
+        
+        <article class="water">
+            <h3>Water: <span class="accent"><?php echo($waterHead); ?></span></h3>
+            <div class="articleContent">
+                <img class="icon" src="images/detail-icon-01.png" alt="">
+                <div>
+                    <p class="info">
+                        <?php echo($waterState); ?>
+                    </p>
+                </div>
+            </div>
+        </article>
+
+        <article class="sunlight">
+            <h3>Sunlight: <span class="accent"><?php echo($brightnessHead); ?></span></h3>
+            <div class="articleContent">
+                <img class="icon" src="images/detail-icon-02" alt="">
+                <div>
+                    <p class="info"><?php echo($brightnessContent); ?></p>
+                </div>
+            </div>
+        </article>
+
+        <article class="temperature">
+            <h3>Temperature: 
+                <span class="accent"><?php echo($tempHead) ?></span>
+            </h3>
+            <div class="articleContent">
+                <img class="icon" src="images/detail-icon-03" alt="">
+                <div>
+                    <p class="info"><?php echo($tempContent) ?></p>
+                </div>
+            </div>
+        </article>
+
+    </main>
+    <script src="js/details.js"></script>
+</body>
+</html>
